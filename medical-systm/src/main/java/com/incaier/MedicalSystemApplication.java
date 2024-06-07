a997a2dd8e2ecf84a94b7dfc27f9c54c5a68c6fd
weijie.cai <wj.cai@foxmail.com>
2024-06-07 11:11:57 +0800

    备注修改

--- medical-systm/src/main/java/com/incaier/MedicalSystemApplication.java
+++ medical-systm/src/main/java/com/incaier/MedicalSystemApplication.java
@@ -1,13 +1,14 @@
 package com.incaier;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MedicalSystemApplication {
 
+@SpringBootApplication
+@MapperScan({"com.incaier.integration.platform.mapper"})
+public class MedicalSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedicalSystemApplication.class, args);
     }
-
 }
